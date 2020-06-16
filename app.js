ac0d3e910a70e9b1fc2dbf21ff3308533a88af6e
Srikanth <srikanth.varma@gmail.com>
2020-06-16 21:16:14 +0530

    Added interactivity to the game

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+let scores, roundScore, activePlayer, dice;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+let diceImg = document.querySelector('.dice');
+diceImg.style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function changeActivePlayer() {
+    let prevActivePlayer = document.querySelector('.player-' + activePlayer + '-panel');
+    prevActivePlayer.classList.remove('active');
+    activePlayer = activePlayer ? 0 : 1;
+    let currentActivePlayer = document.querySelector('.player-' + activePlayer + '-panel');
+    currentActivePlayer.classList.add('active');
+}
+
+document.querySelector(".btn-roll").addEventListener('click', function() {
+    // Generating a random number between 1 and 6.
+    dice = Math.floor(Math.random() * 6) + 1;
+    //Displaying the result
+    diceImg.style.display = 'block';
+    diceImg.src = 'dice-' + dice + '.png';
+    let playerScore = document.querySelector('#current-' + activePlayer);
+    playerScore.textContent = dice.toString();
+    let val = document.querySelector('#score-' + activePlayer);
+    val.textContent = parseInt(val.textContent) + dice;
+    if (dice === 1) {
+        val.textContent = '0';
+        changeActivePlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    changeActivePlayer();
+});
