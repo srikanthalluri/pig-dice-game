45793669c7e35508db41e68ca7e9c8f4dd40f98d
Srikanth <srikanth.varma@gmail.com>
2020-06-17 11:55:56 +0530

    Global scores will be updated only when the user clicks on hold option

--- app.js
+++ app.js
@@ -10,40 +10,64 @@ GAME RULES:
 */
 let scores, roundScore, activePlayer, dice;
 scores = [0, 0];
-roundScore = 0;
+currentScore = 0;
 activePlayer = 0;
-let diceImg = document.querySelector('.dice');
-diceImg.style.display = 'none';
+let diceImg = document.querySelector(".dice");
+diceImg.style.display = "none";
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 function changeActivePlayer() {
-    let prevActivePlayer = document.querySelector('.player-' + activePlayer + '-panel');
-    prevActivePlayer.classList.remove('active');
+    diceImg.style.display = "none";
+    let prevActivePlayer = document.querySelector(
+        ".player-" + activePlayer + "-panel"
+    );
+    prevActivePlayer.classList.remove("active");
     activePlayer = activePlayer ? 0 : 1;
-    let currentActivePlayer = document.querySelector('.player-' + activePlayer + '-panel');
-    currentActivePlayer.classList.add('active');
+    let currentActivePlayer = document.querySelector(
+        ".player-" + activePlayer + "-panel"
+    );
+    currentActivePlayer.classList.add("active");
 }
 
-document.querySelector(".btn-roll").addEventListener('click', function() {
+document.querySelector(".btn-roll").addEventListener("click", function() {
     // Generating a random number between 1 and 6.
     dice = Math.floor(Math.random() * 6) + 1;
+    currentScore += dice;
+    let currentPlayer = document.getElementById("current-" + activePlayer);
+    currentPlayer.textContent = currentScore;
+
     //Displaying the result
-    diceImg.style.display = 'block';
-    diceImg.src = 'dice-' + dice + '.png';
-    let playerScore = document.querySelector('#current-' + activePlayer);
-    playerScore.textContent = dice.toString();
-    let val = document.querySelector('#score-' + activePlayer);
-    val.textContent = parseInt(val.textContent) + dice;
+    diceImg.style.display = "block";
+    diceImg.src = "dice-" + dice + ".png";
+
     if (dice === 1) {
-        val.textContent = '0';
+        currentScore = 0;
+        scores[activePlayer] = 0;
+        currentPlayer.textContent = "0";
+        document.getElementById("score-" + activePlayer).textContent = "0";
         changeActivePlayer();
     }
 });
 
-document.querySelector('.btn-hold').addEventListener('click', function() {
-    changeActivePlayer();
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    scores[activePlayer] += currentScore;
+    document.getElementById("score-" + activePlayer).textContent =
+        scores[activePlayer];
+    currentScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = currentScore;
+    if (scores[activePlayer] >= 20) {
+        document.getElementById("name-" + activePlayer).textContent = "Winner!";
+        diceImg.style.display = "none";
+        let playerPannel = document.querySelector(
+            ".player-" + activePlayer + "-panel"
+        );
+        playerPannel.classList.add("winner");
+        playerPannel.classList.remove("active");
+    } else {
+        changeActivePlayer();
+    }
 });
